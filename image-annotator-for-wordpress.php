0db5cbceac39cca91999dbbe98f3e3bce567fa80
Ar Wai <arwai@live.com>
2025-07-11 10:15:50 -0400

    version update

--- image-annotator-for-wordpress.php
+++ image-annotator-for-wordpress.php
@@ -3,7 +3,7 @@
     Plugin Name: Image Annotator for WordPress
     Plugin URI: https://arwai.me
     Description: A WordPress plugin to manage and annotate images with Annotorious.
-    Version: 0.2.0
+    Version: 0.4.0
     Author: Arwai
 */
 
